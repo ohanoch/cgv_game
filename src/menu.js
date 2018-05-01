abdf94d01e0244cb4cef379a1eadbd3a7c8a828d
ohanoch <ohanoch@gmail.com>
2018-05-01 13:22:04 +0200

    added menu skybox with 1 button

---
+++ src/menu.js
@@ -0,0 +1,66 @@
+"use strict";
+
+class Menu {
+	
+	constructor(cubeSize, skyboxDirectoryURL, buttonDirectoryURL) {
+
+
+		//-------------------------------------- SKYBOX -------------------------------------------------
+        //stolen from: https://jeremypwalton.wordpress.com/2014/09/19/skybox-in-three-js/
+        //other maybe useful link: http://learningthreejs.com/blog/2011/08/15/lets-do-a-sky/
+        if(skyboxDirectoryURL != ""){
+			this.addSkybox(skyboxDirectoryURL, cubeSize);
+		}
+			//------------------------------------------ BUTTONS ---------------------------------------
+		if(buttonDirectoryURL != "")
+			this.buttons = [];
+			this.addButton(buttonDirectoryURL, "resume_button.png");
+
+		console.log("menu created")
+	}
+
+	addSkybox(skyboxDirectoryURL, cubeSize){
+		var textureLoader = new THREE.TextureLoader();
+		var materialArray = [];
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "posx.jpg") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "negx.jpg") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "posy.jpg") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "negy.jpg") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "posz.jpg") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(skyboxDirectoryURL + "negz.jpg") }));
+
+		for (var i = 0; i < 6; i++) {
+			materialArray[i].side = THREE.BackSide;
+		}
+
+		this.skybox = new THREE.Mesh(
+			new THREE.CubeGeometry( cubeSize, cubeSize, cubeSize, 1, 1, 1 ),
+			materialArray
+		);
+
+		this.skybox.position.x = player.position.x;
+		this.skybox.position.y = player.position.y;
+		this.skybox.position.z = player.position.z;
+
+		console.log("added menu skybox");
+	}
+
+	addButton(buttonDirectoryURL, buttonName){
+		var textureLoader = new THREE.TextureLoader();
+		var materialArray = [];
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + "generel_button_side.png") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + "generel_button_side.png") }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + buttonName) }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + buttonName) }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + buttonName) }));
+		materialArray.push(new THREE.MeshBasicMaterial( { map: textureLoader.load(buttonDirectoryURL + buttonName) }));
+
+		var resumeButton = new THREE.Mesh(
+			new THREE.BoxGeometry(4,2,2),
+			materialArray
+		);
+		this.buttons.push(resumeButton);
+
+		console.log("added menu button " + buttonName);
+	}
+}
