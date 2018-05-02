244fe5aa0a83155c08ee54659f46e4fac8707f1a
NiambhB <niambhblundell@gmail.com>
2018-05-02 14:00:12 +0200

    lots of code cleanup

--- src/alpha.js
+++ src/alpha.js
@@ -49,7 +49,7 @@ class Alpha extends THREE.Mesh{
 			var randomZ = Math.random();
 			player.position.set(randomX * (worldMap.width / 2), 10, randomZ * (worldMap.depth / 2));
 			minimapCamera.position.set(randomX * (worldMap.width / 2), 0, randomZ * (worldMap.depth / 2));
-			collissions();
+			collisions();
 			if(this.lives != currLives){
 				this.lives = currLives;
 			} else {
